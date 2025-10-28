2671b7880bdce01fd99cb549d93143b3dd130256
vominhdat68 <datminhvo0901@gmail.com>
2025-10-29 01:37:10 +0700

    configure Redux store(basic state management) and setup React Router with initial routes

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react'
+import { Provider } from 'react-redux'
+import { createRoot } from 'react-dom/client'
+ 
+import AppRoutes from '@/shared/routes/index'
+import { store } from '@/shared/store/redux/reduxStore'
+
+
+const container = document.getElementById("root")!;
+const root = createRoot(container);
+root.render(
+  <StrictMode>
+    <Provider store={store}>
+      <AppRoutes />
+    </Provider>
+  </StrictMode>,
+)
