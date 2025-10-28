bb71545aaeb9e7ad8f36e1958d093397700318a3
vominhdat68 <datminhvo0901@gmail.com>
2025-10-29 01:51:35 +0700

    setup css

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { Provider } from 'react-redux'
 import { createRoot } from 'react-dom/client'
+import '@/index.css'
  
 import AppRoutes from '@/shared/routes/index'
 import { store } from '@/shared/store/redux/reduxStore'
